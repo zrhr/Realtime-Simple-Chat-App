6c9cdb087923a47f05e97d8063d2657e6465089b
andrew cunningham <andrewjasoncunningham@yahoo.com>
2019-06-07 11:48:30 -0700

    added emojis

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const messageForm = document.getElementById('send-container')
 const messageInput =document.getElementById('message-input')
 
 const name=prompt('What is your name?', 'Anon')
+while (name === "") {
+    // user pressed OK, but the input field was empty
+}  if (name) {
+    // user typed something and hit OK
+} else {
+    // user hit cancel
+    name=prompt('What is your name?', 'Anon')
+}
+
 appendMessage('You joined')
 socket.emit('new-user', name)
 
 console.log(name)
+
 
 
 // while(name == ''){
@@ -42,3 +52,16 @@ function appendMessage(message){
     messageElement.innerText = message
     messageContainer.append(messageElement)
 } 
+
+$(function() {
+    // Initializes and creates emoji set from sprite sheet
+    window.emojiPicker = new EmojiPicker({
+      emojiable_selector: '[data-emojiable=true]',
+      assetsPath: '../lib/img/',
+      popupButtonClasses: 'fa fa-smile-o'
+    });
+    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+    // It can be called as many times as necessary; previously converted input fields will not be converted again
+    window.emojiPicker.discover();
+  });
