b3f7d82def18091763bc11e7c5ad627a30907126
andrew cunningham <andrewjasoncunningham@yahoo.com>
2019-06-07 16:06:03 -0700

    added

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
+
 console.log(process.env.PORT)
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || process.env.$PORT || 3000);
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
